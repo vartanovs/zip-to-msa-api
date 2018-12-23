7497cad5ea85f3d17992d08ed69b2349a39452c5
Serge Vartanov <vartanov.s@gmail.com>
2018-12-23 15:52:56 -0800

    Input Controller 100% Test Coverage

--- src/server/tests/inputController.test.ts
+++ src/server/tests/inputController.test.ts
@@ -14,7 +14,7 @@ let request: httpMocks.MockRequest<Request>;;
 let response: httpMocks.MockResponse<Response>;
 let next = jest.fn();
 
-describe('Input Controller - Validate Input Middleware', () => {
+describe('Input Controller - Confirm/Validate Input Middleware', () => {
   beforeEach(() => {
     response = httpMocks.createResponse();
     next.mockClear();
@@ -29,5 +29,57 @@ describe('Input Controller - Validate Input Middleware', () => {
     expect(response.statusCode).toEqual(400);
     expect(JSON.parse(response._getData())).toEqual(errorMessages.zip.noZip);
     expect(next.mock.calls.length).toEqual(0);
-  })
+  });
+
+  it('Providing a "zip" query parameter clears the confirmInput middleware', () => {
+    request = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api',
+      query: {
+        zip: "90210",
+      },
+    });
+    inputController.confirmInput(request, response, next);
+    expect(next.mock.calls.length).toEqual(1);
+  });
+
+  it('Providing a "zip" query parameter not 5 characters long results in an invalidLength error', () => {
+    request = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api',
+      query: {
+        zip: "123",
+      },
+    });
+    inputController.validateInput(request, response, next);
+    expect(response.statusCode).toEqual(400);
+    expect(JSON.parse(response._getData())).toEqual(errorMessages.zip.invalidLength);
+    expect(next.mock.calls.length).toEqual(0);
+  });
+
+  it('Providing a non-number "zip" query parameter results in a zipIsNaN error', () => {
+    request = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api',
+      query: {
+        zip: "1234e",
+      },
+    });
+    inputController.validateInput(request, response, next);
+    expect(response.statusCode).toEqual(400);
+    expect(JSON.parse(response._getData())).toEqual(errorMessages.zip.zipIsNaN);
+    expect(next.mock.calls.length).toEqual(0);
+  });
+
+  it('Providing a 5-digit "zip" query parameter clears validateInput middleware', () => {
+    request = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api',
+      query: {
+        zip: "90210",
+      },
+    });
+    inputController.validateInput(request, response, next);
+    expect(next.mock.calls.length).toEqual(1);
+  });
 })
